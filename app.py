bef0a9da362cc1be2afa7197216fb8d3acb13639
Souptar <jamjoy0@gmail.com>
2021-02-11 15:46:39 +0000

    Add profile page

--- app.py
+++ app.py
@@ -52,6 +52,7 @@ def register():
         # updates session cookie with new user
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -69,6 +70,8 @@ def login():
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(
                         request.form.get("username").capitalize()))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # password incorrext
                 flash("Incorrect Username and/or Password")
@@ -80,6 +83,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # get session user's username
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
