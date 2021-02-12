55bed126123b7fb0d0cea933d92971902b23b2dc
Souptar <jamjoy0@gmail.com>
2021-02-12 16:29:01 +0000

    Add date picker, motion text box and rejected/passed switch to submit page.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+        format: "dd/mm/yyyy",
+        yearRange: [2000, 2021],
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
