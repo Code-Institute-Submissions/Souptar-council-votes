5c88a4b2f13cde3a2b9e0c4a4f3f690772ce37e3
Souptar <jamjoy0@gmail.com>
2021-02-13 14:13:59 +0000

    Add councils dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
+    $('select').formSelect();
     $('.datepicker').datepicker({
         format: "dd/mm/yyyy",
         yearRange: [2000, 2021],
