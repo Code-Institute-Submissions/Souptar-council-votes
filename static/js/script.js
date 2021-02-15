bb1b2303cd70982ded0162702d78f7c33cd9f841
Souptar <jamjoy0@gmail.com>
2021-02-15 17:30:34 +0000

    Initialise tooltip

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('select').formSelect();
+    $('.tooltipped').tooltip();
     $('.datepicker').datepicker({
         format: "yyyy-mm-dd",
         yearRange: [2000, 2021],
